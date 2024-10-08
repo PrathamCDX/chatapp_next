62041ea97cbb3e6aefb56488e14a584888453bac
Pratham <sinhapratham1234@gmail.com>
2024-10-08 13:19:12 +0530

    aded landing page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,12 @@
 import DemoPage from "./demoPage";
 
 export default function Home() {
-  return <div>{/* <DemoPage /> */}</div>;
+  return (
+    <div className="flex items-center justify-center w-[100vw] h-[100vh]">
+      <div className="font-bold text-5xl">Welcome</div>
+    </div>
+  );
+  //  <div>{/* <DemoPage /> */}</div>;
 }
 
 export {};
